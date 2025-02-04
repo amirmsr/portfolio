11ef7803ffb3ef239ab59f7a9b45ff28978abde5
Amir Mansour <amirmansour@mbp-de-amir.home>
2025-02-04 20:22:22 +0100

    new

--- src/components/Photo-Section/PhotoSection.tsx
+++ src/components/Photo-Section/PhotoSection.tsx
@@ -12,7 +12,7 @@ const PhotoSection: React.FC = () => {
               * RER ET STATIONS -2025-
             </div>
             <div className="image-container">
-              <img src="https://gzpauqtc6hrhhsyz.public.blob.vercel-storage.com/section%20photo%20rer%20galery/DSCN0070-min-iO7nBJWbGa9tH9oHuRbuqX7LpwGz9L.webp" alt="RER" className="link-image" />
+              <img src=    "https://gzpauqtc6hrhhsyz.public.blob.vercel-storage.com/section%20photo%20rer%20galery/DSCN0060-min-DzYQM9IGGlbbfrP2Hnp7vndKSOCFGJ.webp" alt="RER" className="link-image" />
             </div>
           </Link>
         </div>
