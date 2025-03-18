8855149c26fa288810c3efee68af25958ac5a167
Amir Mansour <amirmansour@mbp-de-amir.home>
2025-03-18 19:30:42 +0100

    new

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,20 +8,20 @@ const About: React.FC = () => {
         <p style={{ fontSize: "1.7rem" }}>About</p>
       </div>
         <div className="about-content">
-          <section className="bio-section mb-5">
-            <p>
-              Born in 2001 in Paris, I am a software engineer currently based in Paris. 
-              My passions are visual arts, photography 
-              and design.
-            </p>
-            <p>
-              In my photographic work, I am drawn to minimalism and the intricate details of 
-              materials and textures. I find beauty in simplicity and the subtle interplay of light 
-              and form. My approach to photography focuses on capturing these essential elements,
-              stripping away the unnecessary to reveal the fundamental nature of my subjects.
-              I'm using a Sony RX100, iPhone 11, Nikon Coolpix 3600s.
-            </p>
-          </section>
+        <section className="bio-section mb-5">
+          <p>
+            Né en 2001 à Paris, je suis ingénieur logiciel actuellement basé à Paris.
+            Mes passions sont les arts visuels, la photographie et le design.
+          </p>
+          <p>
+            Dans mon travail photographique, je suis attiré par le minimalisme et les détails complexes des
+            matériaux et des textures. Je trouve la beauté dans la simplicité et le jeu subtil de la lumière
+            et de la forme. Mon approche de la photographie se concentre sur la capture de ces éléments essentiels,
+            en éliminant le superflu pour révéler la nature fondamentale de mes sujets.
+            J'utilise un Sony RX100, un iPhone 11 et un Nikon Coolpix 3600s. 
+          </p>
+        </section>
+
 
           <section className="contact-section mb-5">
             <h3 className="mb-3">Contact</h3>
